5110d7fc7a80617082e83c0c66c2d81c95c7bd79
Lureithis <44930142+Lureithis@users.noreply.github.com>
2019-10-25 23:37:40 +0200

    InspectObjectsUpdate
    SpookyScarySkeleton

---
+++ Assets/InspectItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InspectItem : MonoBehaviour
+{
+    public GameObject screenToShowOnTrigger;
+    public GameObject infoAboutObject;
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            this.screenToShowOnTrigger.SetActive(true);
+           
+        }
+            
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            this.screenToShowOnTrigger.SetActive(false);
+            this.infoAboutObject.SetActive(false);
+        }
+            
+    }
+
+    private void Update()
+    {
+        if(screenToShowOnTrigger.active == true && Input.GetKeyDown(KeyCode.E))
+        {
+            this.infoAboutObject.SetActive(true);
+        }
+    }
+}
