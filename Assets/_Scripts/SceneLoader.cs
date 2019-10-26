8273bf152a976b50213cfef6ebc8bbd607bf700b
Lureithis <44930142+Lureithis@users.noreply.github.com>
2019-10-26 21:20:24 +0200

    SceneManagement
    Changing scenes through doors

---
+++ Assets/_Scripts/SceneLoader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject LockedText;
+    public bool isLocked;
+    public int sceneToLoad;
+
+    private bool isInTrigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isInTrigger = true;   
+    }
+
+    IEnumerator showLockedText()
+    {
+        LockedText.SetActive(true);
+        yield return new WaitForSecondsRealtime(3f);
+        LockedText.SetActive(false);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isInTrigger = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isInTrigger)
+        {
+            if (isLocked)
+            {
+                StartCoroutine(showLockedText());
+            }
+
+            else
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+}
