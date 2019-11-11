829fc1d50e305247a3759d6b701769f4f2b17113
Lureithis <44930142+Lureithis@users.noreply.github.com>
2019-11-11 18:03:06 +0100

    CleanUp

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -7,21 +7,30 @@ public class Player : MonoBehaviour
     [SerializeField] GameObject gameManager;
     [SerializeField] GameObject InsanityBar;
     private bool isInHallucination;
-   
+    private InsanityMode insanitymode;
+    private InsanityBar insanitybar;
+
+    private void Start()
+    {
+        insanitymode = gameManager.GetComponent<InsanityMode>();
+        insanitybar = InsanityBar.GetComponent<InsanityBar>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Q) && isInHallucination == false)
         {
             isInHallucination = true;
-            gameManager.GetComponent<InsanityMode>().InsanityModeActivated();
-            InsanityBar.GetComponent<InsanityBar>().isInHallucinationChange();
+            insanitymode.InsanityModeActivated();
+            // InsanityBar.GetComponent<InsanityBar>().isInHallucinationChange();
+            insanitybar.isInHallucinationChange();
         }
         else if (Input.GetKeyDown(KeyCode.Q) && isInHallucination == true)
         {
             isInHallucination = false;
-            gameManager.GetComponent<InsanityMode>().InsanityModeDeactivated();
-            InsanityBar.GetComponent<InsanityBar>().isInHallucinationChange();
+            insanitymode.InsanityModeDeactivated();
+            insanitybar.isInHallucinationChange();
         }
     }
 }
