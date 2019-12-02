30f2b63419eae5db40769815bed2452f8f93c73c
Lureithis <44930142+Lureithis@users.noreply.github.com>
2019-12-02 19:17:57 +0100

    Rat

---
+++ Assets/triggerRat.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerRat : MonoBehaviour
+{
+    [SerializeField] GameObject Rat;
+    [SerializeField] GameObject start;
+    [SerializeField] GameObject end;
+    
+    private Vector3 startVector;
+    private Vector3 endVector;
+    private RatTriggerMovement RatScript;
+
+    bool Triggered = false;
+    bool firstTime = true;
+
+    void Start()
+    {
+        endVector = end.transform.position;
+        startVector = start.transform.position;
+        RatScript = Rat.GetComponent<RatTriggerMovement>();
+    }
+
+    private void Update()
+    {
+        if(Triggered == true)
+        {
+            if(firstTime == true)
+            {
+                Rat.transform.position = startVector;
+                firstTime = false;
+                StartCoroutine(DestroyRat());
+            }
+            
+            RatScript.Run(endVector);
+        }
+    }
+
+    IEnumerator DestroyRat()
+    {
+        yield return new WaitForSecondsRealtime(10f);
+        //Destroy(gameObject);
+        Destroy(Rat);
+        this.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(Triggered == false)
+        {
+            if (collision.tag == "Player")
+            {
+                Triggered = true;
+            }
+        }
+    }
+}
